62d78f3adc633dc8238180efab041bd1e4e31e8c
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2020-12-28 20:11:56 +0530

    Segregate file segments

--- src/WellIO.jl
+++ src/WellIO.jl
@@ -1,5 +1,20 @@
 module WellIO
 
-# Write your package code here.
+using FileIO
+using Tables
+
+function load(filepath::String)
+    file = FileIO.query(filepath)
+    data = open(file) do f
+           read(f, String)
+        end
+    version_block = match(r"~V(.*?)~"s, data)
+    wellinfo_block = match(r"~W(.*?)~"s, data)
+    curveinfo_block = match(r"~C(.*?)~"s, data)
+    otherinfo_block = match(r"~O(.*?)~"s, data)
+    datatable_block = match(r"~A(.*?)\Z"s, data)
+    return(version_block, wellinfo_block, curveinfo_block, otherinfo_block, datatable_block)
+end
+
 
 end
