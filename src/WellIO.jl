0a2620e55a0e1ff8b99c9d0f8b185d82214a6258
Shubhank <saxena.shubhank.19@gmail.com>
2021-01-03 11:00:54 +0530

    Add data struct for return type Split header data for small read time

--- src/WellIO.jl
+++ src/WellIO.jl
@@ -6,14 +6,27 @@ using Tables
 function load(filepath::String)
     file = FileIO.query(filepath)
     data = open(file) do f
-           read(f, String)
+            read(f, String)
         end
-    version_block = match(r"~V(.*?)~"s, data)
-    wellinfo_block = match(r"~W(.*?)~"s, data)
-    curveinfo_block = match(r"~C(.*?)~"s, data)
-    otherinfo_block = match(r"~O(.*?)~"s, data)
-    datatable_block = match(r"~A(.*?)\Z"s, data)
-    return(version_block, wellinfo_block, curveinfo_block, otherinfo_block, datatable_block)
+    #Splitting the data into table and rest of information
+    datatable_block =match(r"~A(.*?)\Z"s, data)
+    info_block = data[1:datatable_block.offset]
+    #Extracting specific information from info_block
+    version_block = match(r"~V(.*?)~"s, info_block)
+    wellinfo_block = match(r"~W(.*?)~"s, info_block)
+    curveinfo_block = match(r"~C(.*?)~"s, info_block)
+    otherinfo_block = match(r"~O(.*?)~"s, info_block)
+    #Structure Data 
+    information = Segments(version_block,wellinfo_block,curve_info_block,otherinfo_block,datatable_block)
+    return(information)
+end
+
+mutable struct Segments
+    version::String
+    wellinfo::String
+    curveinfo::String
+    optionalinfo::String
+    tabledata::String
 end
 
 
